bbe51b5c4e6ed86041d64168ce206d6021f257ab
Peter Jun Park <park@hey.com>
2024-01-29 06:28:08 -0800

    add player movement

---
+++ scenes/player/Player.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace _2DSurvivors.Player;
+public partial class Player : CharacterBody2D
+{
+    private const int MaxSpeed = 200;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        Vector2 movementVector = GetMovementVector();
+        Vector2 direction = movementVector.Normalized();
+        Velocity = direction * MaxSpeed;
+        // Tell the CharacterBody2D to move based on Velocity.
+        MoveAndSlide();
+    }
+
+    private static Vector2 GetMovementVector()
+    {
+        // With keyboard controls, ActionStrength is binary. It's 0 or 1.
+        // On a joystick, it returns a float between 0 and 1.
+        var xMovement = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        var yMovement = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+
+        return new Vector2(xMovement, yMovement);
+    }
+}
